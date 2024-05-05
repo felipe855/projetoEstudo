61f2f6ede5cfcf34115beb9c58755a33832884b1
Felipe Justiniano <felipejustiniano19@outlook.com>
2024-05-05 15:23:21 -0300

    Projeto simples envolvendo algumas logicas estudadas

---
+++ src/entities/Hourcontract.java
@@ -0,0 +1,48 @@
+package entities;
+
+import java.util.Date;
+
+public class Hourcontract {
+    private Date date;
+    private Double ValuePerHour;
+    private Integer hours;
+
+    public Hourcontract() {
+
+    }
+
+    public Hourcontract(Date date, Double valuePerHour, Integer hours) {
+        this.date = date;
+        ValuePerHour = valuePerHour;
+        this.hours = hours;
+
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Double getValuePerHour() {
+        return ValuePerHour;
+    }
+
+    public void setValuePerHour(Double valuePerHour) {
+        ValuePerHour = valuePerHour;
+    }
+
+    public Integer getHours() {
+        return hours;
+    }
+
+    public void setHours(Integer hours) {
+        this.hours = hours;
+    }
+
+    public double totalValue(){
+        return ValuePerHour * hours;
+    }
+}
